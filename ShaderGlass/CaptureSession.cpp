ea2e42c3a95fd76faad3673e79a3cdf850aaa22b
FayeVrans <53041868+FayeVrans@users.noreply.github.com>
2026-01-13 20:45:29 +0800

    Adjust minInterval to a constant value of 8ms

--- ShaderGlass/CaptureSession.cpp
+++ ShaderGlass/CaptureSession.cpp
@@ -51,10 +51,8 @@ CaptureSession::CaptureSession(winrt::IDirect3DDevice const&     device,
         try
         {
             // max 250Hz?
-            const auto minInterval = maxCaptureRate ? std::chrono::milliseconds(4) : std::chrono::milliseconds(15);
+            const auto minInterval = maxCaptureRate ? std::chrono::milliseconds(8) : std::chrono::milliseconds(8);
             m_session.MinUpdateInterval(winrt::Windows::Foundation::TimeSpan(minInterval));
-            if(maxCaptureRate)
-                m_session.MinUpdateInterval(winrt::Windows::Foundation::TimeSpan(0));
         }
         catch(...)
         { }
